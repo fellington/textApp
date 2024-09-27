c0df6b53ad7240cfedad5342ae97906db0469c99
fellington <44418534+fellington@users.noreply.github.com>
2024-09-26 18:20:54 -0700

    Removed login

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './app/screens/Login';
 import HomePage from './app/screens/HomePage';
 import PostPartumInstr from './app/screens/PostPartumInstr';
 import Support from './app/screens/Support';
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
-import { FIREBASE_AUTH } from './FirebaseConfig';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -16,36 +11,19 @@ const InsideStack = createNativeStackNavigator();
 function InsideLayout() {
   return (
     <InsideStack.Navigator>
-      <InsideStack.Screen name = "Home Page" component={HomePage} options={{headerShown: false}}/>
+      <InsideStack.Screen name="Home Page" component={HomePage} options={{ headerShown: false }} />
       <InsideStack.Screen name = "postpartuminstr" component={PostPartumInstr} options={{headerShown: false}}/>
-      <InsideStack.Screen name = "support" component={Support} options={{headerShown: false}}/>
+      <InsideStack.Screen name="support" component={Support} options={{ headerShown: false }} />
     </InsideStack.Navigator>
-  )
+  );
 }
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      console.log('user', user);
-      setUser(user);
-    });
-
-    return () => unsubscribe();
-  }, []);
-  
   return (
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName = 'Login'>
-        {user ? (
-          <Stack.Screen name='Inside' component={InsideLayout} options ={{headerShown: false}}/>
-        ) : (
-          <Stack.Screen name='Login' component={Login} options ={{headerShown: false}}/>
-        )}    
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inside">
+        <Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
--- app/screens/HomePage.tsx
+++ app/screens/HomePage.tsx
@@ -1,74 +1,56 @@
 import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import React from 'react';
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
 import { NavigationProp } from '@react-navigation/native';
 
 interface RouterProps {
-    navigation: NavigationProp<any, any>;
+  navigation: NavigationProp<any, any>;
 }
 
-const HomePage = ({navigation}: RouterProps) => {
+const HomePage = ({ navigation }: RouterProps) => {
 
-    const openPretest = () => {
-        const url = 'https://ci-redcap.hs.uci.edu/surveys/?s=9MTR98DWKDCYA78W';
-        Linking.openURL(url).catch((err) => console.error("Couldn't load page", err));
-    };
+  const openPretest = () => {
+    const url = 'https://ci-redcap.hs.uci.edu/surveys/?s=9MTR98DWKDCYA78W';
+    Linking.openURL(url).catch((err) => console.error("Couldn't load page", err));
+  };
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <TouchableOpacity onPress={openPretest} style={styles.btn}>
-                <Text style={styles.btnText}>
-                    Start here: Pretest
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {
-                navigation.navigate('postpartuminstr');
-            }} style={styles.btn}> 
-                <Text style={styles.btnText}>
-                    Preenclampsia Education 
-                </Text> 
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {
-                navigation.navigate('support');
-            }} style={styles.btn}> 
-                <Text style={styles.btnText}>
-                    Support
-                </Text> 
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {
-                FIREBASE_AUTH.signOut();
-            }} style={styles.btn}> 
-                <Text style={styles.btnText}>
-                    Logout
-                </Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity onPress={openPretest} style={styles.btn}>
+            <Text style={styles.btnText}>Start here: Pretest</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {navigation.navigate('postpartuminstr'); }} style={styles.btn}> 
+            <Text style={styles.btnText}>Preenclampsia Education</Text> 
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { navigation.navigate('support'); }} style={styles.btn}>
+            <Text style={styles.btnText}>Support</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default HomePage;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    btn: {
-        width: "80%",
-        backgroundColor: "#ff6347",
-        borderRadius: 25,
-        height: 80,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 40,
-        marginBottom: 10,
-    },
-    btnText: {
-        fontSize: 30,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        color: 'white',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    width: "80%",
+    backgroundColor: "#ff6347",
+    borderRadius: 25,
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  btnText: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
